62d17428fe1b77d84c9611ce6dde0d2599a3d23b
xone-ar <uxone.ar@gmail.com>
2024-12-04 22:47:47 -0300

    Add files via upload

---
+++ js/sw.js
@@ -0,0 +1,44 @@
+// Archivo: sw.js
+const CACHE_NAME = "v1_cache";
+const urlsToCache = [
+  "/",
+  "../index.html",
+  "../css/style.css",
+  "script.js",
+  "../img/logo2.svg",
+  "../img/logo2.svg"
+];
+
+// Instalación del Service Worker
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Interceptar y responder solicitudes
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Activación del Service Worker
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
